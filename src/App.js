0a9243c640f7fdd5471d680d5cc4bc8ce1840af3
Emmanuel <ejaramilo@hotmail.com>
2022-11-15 10:18:00 -0500

    291 upgrading to v6 fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// example of react router dom
+
+// our-domain.com/ => component A
+// our-domain.com/products => component B
+
+// this is what react router does
+
+// so far, this implementation has a flaw, the states are lost because a request is made
+// so we lose any state saved in the browser
+// so we need to use the Link function of react router
